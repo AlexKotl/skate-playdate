58ba7e4c8e99ae75c84f463535925ea2d80c94a9
Oleksandr Kotliarov <oleksandrkotliarov@marc-o-polo.com>
2023-01-04 22:04:52 +0100

    Add sprite animation

--- source/main.lua
+++ source/main.lua
@@ -2,13 +2,22 @@ import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
+import 'libs/AnimatedSprite.lua'
 
-import "skater"
+-- import "skater"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
-Skater()
+local imagetable = gfx.imagetable.new("assets/skater")
+
+sprite = AnimatedSprite.new(imagetable) 
+sprite:addState("idle", 1, 13, {tickStep = 3}) 
+sprite:playAnimation() 
+sprite:moveTo(50, 50)
+
+
+-- Skater()
 
 function playdate.update()
 	gfx.sprite.update()
