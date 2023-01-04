962ef2d1a445b457b2215525db6bb2feb08333a3
Oleksandr Kotliarov <oleksandrkotliarov@marc-o-polo.com>
2023-01-04 22:10:31 +0100

    Move sparite animation to class logic

--- source/main.lua
+++ source/main.lua
@@ -2,22 +2,12 @@ import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
-import 'libs/AnimatedSprite.lua'
-
--- import "skater"
+import "skater"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
-local imagetable = gfx.imagetable.new("assets/skater")
-
-sprite = AnimatedSprite.new(imagetable) 
-sprite:addState("idle", 1, 13, {tickStep = 3}) 
-sprite:playAnimation() 
-sprite:moveTo(50, 50)
-
-
--- Skater()
+Skater()
 
 function playdate.update()
 	gfx.sprite.update()
--- source/skater.lua
+++ source/skater.lua
@@ -1,15 +1,35 @@
+import 'libs/AnimatedSprite.lua'
+
+local pd <const> = playdate
 local gfx <const> = playdate.graphics
 
-class("Skater").extends(gfx.sprite)
+class("Skater").extends(AnimatedSprite)
 
 function Skater:init()
-    self:setImage(gfx.image.new("images/skater"))
-    self:moveTo(100, 100)
-    self:add()
+    local imagetable = gfx.imagetable.new("assets/skater")
+    Skater.super.init(self, imagetable)
 
+    self:addState("idle", 1, 13, {tickStep = 3}) 
+    self:playAnimation() 
+    self:moveTo(100, 100)
     self:setCollideRect(0, 0, self:getSize())
+    
+    self.speed = 0
 end
 
 function Skater:update()
+    if pd.buttonIsPressed(pd.kButtonRight) then
+        self.speed += 2
+    elseif pd.buttonIsPressed(pd.kButtonLeft) then
+        self.speed = 0
+    end
 
+    self.speed -= 0.5
+
+    if self.speed < 0 then
+        self.speed = 0
+    end
+
+    self:moveBy(self.speed, 0)
+    self:updateAnimation()
 end
